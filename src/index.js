bd6efc5d59b210facb02f5bea6827e756575a6f7
Mohammed Shoaib Gudgunti <shoaib.rvc@gmail.com>
2017-05-06 19:12:21 +0530

    mock api and other

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
-import './index.css';
-import numeral from 'numeral';
+import {getUsers, deleteUser} from './api/userApi';
 
-/* eslint-disable no-console */
+// Populate table of users via API call.
+getUsers().then(result => {
+  let usersBody = "";
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awsome course!`);
+  result.forEach(users => {
+    usersBody += `<tr>
+    <td><a href="#" data-id="${users.id}" class="deleteUser">Delete</a></td>
+    <td>${users.id}</td>
+    <td>${users.firstName}</td>
+    <td>${users.lastName}</td>
+    <td>${users.email}</td>
+    </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must use array form to create a real array from a DOM collection
+  // getElementsByClassname only returns an "array like" object.
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+});
